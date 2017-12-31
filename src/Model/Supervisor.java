cad693dad6a86a88444b0a66af6b2ee10f2f1698
JHodorHodor <34662208+JHodorHodor@users.noreply.github.com>
2017-12-31 23:43:26 +0100

    Update Supervisor.java

--- src/Model/Supervisor.java
+++ src/Model/Supervisor.java
@@ -11,6 +11,8 @@ public class Supervisor {
     private Model.Class given_class;
     private List<Card> hip;
 
+//setting
+
     public Supervisor(Integer whose_move, Integer players_quant) {
         this.whose_move = whose_move;
         this.players_quant = players_quant;
@@ -39,16 +41,37 @@ public class Supervisor {
     public void setPlayers() {
         //a tu graczy
     }
+    
+//game
 
     public void shuffleDeck() {
         Collections.shuffle(hip);
+    }
+    
+    public void gameBegin() {
+        this.whose_move = 0;
+        for (Integer i = 0; i < players_quant; i++) {
+            Player player = players.get(i);
+            this.draw(5, player);
+        }
+    }
+
+    public Integer game() {
+        this.gameBegin();
+        Boolean no_winner = true;
+        Player winner = null;
+        while (no_winner) {
+            this.playTurn(/*tu sie coś wpisze jak będą funkcje*/);
+            //jakaś funkcja, która sprawdza, czy gracz, który grał ma jeszcze karty, jeśli nie to przypisuje go do winnera.
+        }
+        return winner.getNumber();
     }
 
     public Boolean playTurn(Boolean if_forced, Integer what_points_forcing) {
         Player player = players.get(this.whose_move);
         Integer type = player.whatMove();
         if (if_forced) {
-            //tu by trzeba te wszystkie green/red/orc pointsy wtzucić
+            //tu by trzeba te wszystkie green/red/orc pointsy wrzucić
         } else {
             if (type == 1) {
                 return this.ordinaryMove(player);
@@ -61,10 +84,6 @@ public class Supervisor {
             }
         }
         return true;
-    }
-
-    public Boolean checkIfOrdinaryAllowed(Card card) {
-        return (card.getColour() == given_colour || card.getCls() == given_class);
     }
 
     public void draw(Integer quantity, Player player) {
@@ -99,23 +118,47 @@ public class Supervisor {
             return false;
         }
     }
-
-    public void gameBegin() {
-        this.whose_move = 0;
-        for (Integer i = 0; i < players_quant; i++) {
-            Player player = players.get(i);
-            this.draw(5, player);
-        }
+    
+    public Boolean checkIfOrdinaryAllowed(Card card) {
+        return (card.getColour() == given_colour || card.getCls() == given_class);
     }
 
-    public Integer game() {
-        this.gameBegin();
-        Boolean no_winner = true;
-        Player winner = null;
-        while (no_winner) {
-            this.playTurn(/*tu sie coś wpisze jak będą funkcje*/);
-            //jakaś funkcja, która sprawdza, czy gracz, który grał ma jeszcze karty, jeśli nie to przypisuje go do winnera.
+//special
+    
+    public void waranTransposition(Player player1, Player player2) {
+        private List<Card> aux;
+        
+        for(Integer i = 0; i < player2.quant_of_cards; i++){
+            aux.add(player2.showACard(0));
+            player2.moveAllowed(player2.showACard(0));
         }
-        return winner.getNumber();
+        for(Integer i = 0; i < player1.quant_of_cards; i++){
+            player2.draw(player1.showACard(0));
+            player1.moveAllowed(player1.showACard(0));
+        }
+        for(Integer i = 0; i < player2.quant_of_cards; i++){
+            player1.draw(aux.get(i));
+        }
     }
+    
+    public void waranPermutation(Player master, Player giver, Player receiver) {
+        Boolean stop = true;
+        while(stop){
+            //pytamy gracza master, czy chce wykonać transpozycje
+            if(stop){
+                //pytamy jakich graczy, on nam wpisuje, że player1 i player2.
+                this.waranTransposition(player1, player2);
+            }
+        }
+    }
+    
+    public void giveCards(Player giver, Player, receiver, Integer quant_given) {
+        giver.shuffleHand();
+        for(Integer i = 0; i < quant_given; i++){
+            receiver.draw(giver.showACard(0));
+            giver.moveAllowed(giver.showACard(0));
+        }
+    }
+    
 }
+
