43226f75e1dbe7cf0c253ea895f7250011164cc1
Rafal Byczek <rafal.byczek@student.uj.edu.pl>
2018-01-05 22:20:58 +0100

    .gitignore is now working

--- out/production/SuperZwierzaki/Database/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while read p; do
-	IFS=',' read -r -a array <<< "$p"
-	text='this.insertCard('"${array[0]}"','"${array[1]}"', "'"${array[2]}"'", "'"${array[3]}"'", "'"${array[4]}"'", "'"${array[5]}"'");'
-	echo $text;
-done < cardsWithNone.txt
