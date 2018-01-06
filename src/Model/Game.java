4889208ec77af5afdee9dcd77247d71fafeeae3a
Jedrzej Hodor <jedrzej.hodor@student.uj.edu.pl>
2018-01-06 18:04:17 +0100

    Main

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,4 +1,16 @@
 package Model;
 
+/**
+ * Created by Jędrzej Hodor on 06.01.2018.
+ */
+
 public class Game {
+    public static void main(String[] args) {
+        Supervisor supervisor = new Supervisor(0, null, null,
+                0, 0, 0, 0, 0, null, 0);
+        String winner;
+        winner = supervisor.game();
+        System.out.println("The winner is:" + winner);
+        System.out.println("Congratulation for all participants ;)");
+    }
 }
